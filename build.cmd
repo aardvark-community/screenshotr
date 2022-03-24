00378512c71451f3ee1c81ee1f9aff720d1edb65
Stefan Maierhofer <sm@stefanmaierhofer.com>
2022-03-24 17:27:04 +0100

    add github workflow (cont.)

--- build.cmd
+++ build.cmd
@@ -5,6 +5,6 @@ PUSHD %~dp0
 dotnet tool restore
 dotnet paket restore
 
-dotnet build src/screenshotr.sln --configuration Release
+dotnet build src/screenshotr.sln
 
 dotnet publish -c Release -o publish/screenshotr src/screenshotr
