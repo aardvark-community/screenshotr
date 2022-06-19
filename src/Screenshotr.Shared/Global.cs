be83f483f70ec7038de8ce7017f5a7dbd43cf96f
Stefan Maierhofer <sm@stefanmaierhofer.com>
2022-06-19 17:45:52 +0200

    prefixed api endpoints with "/api/1.0"; update api version to 1.0.2

--- src/Screenshotr.Shared/Global.cs
+++ src/Screenshotr.Shared/Global.cs
@@ -5,11 +5,11 @@ public static class Global
     /// <summary>
     /// Current version string.
     /// </summary>
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
 
-    public const string ApiPathGetStatus                = "/status";
-    public const string ApiPathGetScreenshotsSegmented  = "/screenshots/segment";
-    public const string ApiPathImportScreenshot         = "/screenshots/import";
-    public const string ApiPathUpdateScreenshot         = "/screenshots/update";
-    public const string ApiPathGetScreenshot            = "/screenshots/get";
+    public const string ApiPathGetStatus                = "/api/1.0/status";
+    public const string ApiPathGetScreenshotsSegmented  = "/api/1.0/screenshots/segment";
+    public const string ApiPathImportScreenshot         = "/api/1.0/screenshots/import";
+    public const string ApiPathUpdateScreenshot         = "/api/1.0/screenshots/update";
+    public const string ApiPathGetScreenshot            = "/api/1.0/screenshots/get";
 }
